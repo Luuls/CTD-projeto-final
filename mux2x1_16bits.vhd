96a360616734eb980dc0f24e91b222e6b77df158
Luuls <luan13467@hotmail.com>
2023-06-23 10:57:04 -0300

    criei os mux's declarados no datapath.vhd

---
+++ mux2x1_16bits.vhd
@@ -0,0 +1,17 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity mux2x1_16bits is
+port(
+	E0, E1: in std_logic_vector(15 downto 0);
+	sel: in std_logic;
+	saida: out std_logic_vector(15 downto 0)
+);
+end mux2x1_16bits;
+
+architecture circuit of mux2x1_16bits is
+    
+begin
+    saida <= E0 when sel = '0' else
+             E1;
+end circuit;
