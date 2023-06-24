93629c3c424af5e805538b72da94f54c20225241
light <caiocr610@gmail.com>
2023-06-24 18:35:47 -0300

    subtrator

--- half_sub.vhd
+++ half_sub.vhd
@@ -1,14 +1,20 @@
-library ieee;
-use use ieee.std_logic_1164.all;
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_arith.all;
+use IEEE.std_logic_unsigned.all;
 
-entity half_sub is
-    port (A, B: in std_logic;
-    dif, bo: out_std_logic
+entity of half_sub is
+    port(enable: in std_logic;
+    number: in std_logic_vector(3 downto 0);
+    result: out std_logic_vector(3 downto 0)
     );
 end half_sub;
 
 architecture circuit of half_sub is
-    begin
-        dif <= a xor b;
-        bo <= (not a) and b;
-end circuit;
+    signal negative_enable, enable_4bit: std_logic_vector(3 downto 0);
+
+begin
+    enable_4bit <= "000" & enable;
+    negative_enable <= (not enable_4bit) + "0001";
+    result <= negative_enable + number;
+end circuit; 
