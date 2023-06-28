7460139985958f6bede569cba227d3c1a3d35046
Luuls <luan13467@hotmail.com>
2023-06-27 23:32:13 -0300

    mudei o nome do subtrator

--- subtracao.vhd
+++ subtracao.vhd
@@ -3,15 +3,15 @@ use IEEE.std_logic_1164.all;
 use IEEE.std_logic_arith.all;
 use IEEE.std_logic_unsigned.all;
 
-entity of subtrator is
+entity of subtracao is
 port(
     enable: in std_logic;
     number: in std_logic_vector(3 downto 0);
     result: out std_logic_vector(3 downto 0)
 );
-end subtrator;
+end subtracao;
 
-architecture circuit of subtrator is
+architecture circuit of subtracao is
     signal negative_enable, enable_4bit: std_logic_vector(3 downto 0);
 
 begin
