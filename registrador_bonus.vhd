dd7377e7983e6317c2a35372e3c891c3928cc37e
Luuls <luan13467@hotmail.com>
2023-06-27 22:54:34 -0300

    mudei o nome dos arquivos de registradores

--- registrador_bonus.vhd
+++ registrador_bonus.vhd
@@ -1,26 +1,26 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity reg_4bits is
+entity registrador_bonus is
 port(
-    in_reg_4bits: in std_logic_vector(3 downto 0);
+    in_reg_bonus: in std_logic_vector(3 downto 0);
     load: in std_logic;
     reset: in std_logic;
     clk: in std_logic;
-    out_reg_4bits: out std_logic_vector(3 downto 0)
+    out_reg_bonus: out std_logic_vector(3 downto 0)
 );
-end reg_4bits;
+end reg_bonus;
 
-architecture circuit of reg_4bits is
+architecture circuit of registrador_bonus is
 begin
     process(clk, reset) is --verificar identação
     begin 
         if (reset = '1') then
-            out_reg_4bits <= "0000";
+            out_reg_bonus <= "1000";
 
         elsif rising_edge(clk) then
             if(load='1') then
-                out_reg_4bits <= in_reg_4bits;
+                out_reg_bonus <= in_reg_bonus;
             end if;
         end if;
     end process;
--- registrador_user.vhd
+++ registrador_user.vhd
@@ -1,26 +1,26 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity reg_15bits is
+entity registrador_user is
 port(
-    in_reg_15bits: in std_logic_vector(14 downto 0);
+    in_reg_user: in std_logic_vector(14 downto 0);
     load: in std_logic;
     reset: in std_logic;
     clk: in std_logic;
-    out_reg_15bits: out std_logic_vector(14 downto 0)
+    out_reg_sel: out std_logic_vector(14 downto 0)
 );
-end reg_15bits;
+end registrador_user;
 
-architecture circuit of reg_15bits is
+architecture circuit of registrador_user is
 begin
     process(clk, reset) is --verificar identação
     begin 
         if (reset = '1') then
-            out_reg_15bits <= "000000000000000";
+            out_reg_sel <= "000000000000000";
 
         elsif rising_edge(clk) then
             if(load='1') then
-                out_reg_15bits <= in_reg_15bits;
+                out_reg_sel <= in_reg_user;
             end if;
         end if;
     end process;
