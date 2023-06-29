869bcfcadf01667eb3c43772acd945110d0f8f80
Luuls <luan13467@hotmail.com>
2023-06-28 23:16:32 -0300

    feito o topo

--- topo.vhd
+++ topo.vhd
@@ -53,5 +53,49 @@ component ButtonSync is port( --saida ja negada
 end component;
 
 begin
+    clock <= CLK_500Hz;
 
+    CONTROL: controle port map(
+        enter,
+        reset,
+        clock,
+        end_game,
+        end_time,
+        end_round,
+        end_FPGA,
+        R1,
+        R2,
+        E1,
+        E2,
+        E3,
+        E4,
+        E5
+    );
+
+    DATAPATH_DEVICE: datapath port map(
+        clock,
+        sw(17 downto 0),
+        R1,
+        R2,
+        E1,
+        E2,
+        E3,
+        E4,
+        E5,
+        hex0,
+        hex1,
+        hex2,
+        hex3,
+        hex4,
+        hex5,
+        hex6,
+        hex7,
+        ledr(15 downto 0),
+        end_game,
+        end_time,
+        end_round,
+        end_FPGA
+    );
+    
+    BUTTON_SYNC: ButtonSync port map(KEY(1), KEY(0), clock, enter, reset);
 end circuito;
